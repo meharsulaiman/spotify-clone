429df77769923f212fb930c31b9cf10bca30cf1a
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-01-11 18:13:44 +0500

    login signup modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import Sidebar from '@/components/Sidebar';
 import SupabaseProvider from '@/providers/SupabaseProvider';
 import USerProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <USerProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </USerProvider>
         </SupabaseProvider>
